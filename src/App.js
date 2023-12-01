cf8d0dc5e2edc6a87151f985f8026d2a713fad71
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-01 16:41:16 +0300

    adds a route to login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import { Route } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
       <h1>Client Auth Projesi: Friends</h1>
+      <Route path="/">
+        <Login />
+      </Route>
     </div>
   );
 }
