7d7b248931845674417a542d1f3746a3962367c5
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-02 12:27:51 +0300

    adds routing to friendList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Route } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
+import FriendsList from "./pages/FriendsList";
 
 function App() {
   return (
     <div className="App">
       <h1>Client Auth Projesi: Friends</h1>
-      <Route path="/">
+      <Route exact path="/">
         <Login />
+      </Route>
+      <Route path="/friends">
+        <FriendsList />
       </Route>
     </div>
   );
