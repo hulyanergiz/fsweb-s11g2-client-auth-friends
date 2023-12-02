96064d7e17b38948d6ff812cfb31793b744c9282
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-02 13:08:41 +0300

    wraps routes with switch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
 import FriendsList from "./pages/FriendsList";
@@ -7,16 +7,17 @@ import AddFriend from "./pages/AddFriend";
 function App() {
   return (
     <div className="App">
-      <h1>Client Auth Projesi: Friends</h1>
-      <Route exact path="/">
-        <Login />
-      </Route>
-      <Route exact path="/friends">
-        <FriendsList />
-      </Route>
-      <Route path="/friends/add">
-        <AddFriend />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/friends">
+          <FriendsList />
+        </Route>
+        <Route path="/friends/add">
+          <AddFriend />
+        </Route>
+      </Switch>
     </div>
   );
 }
